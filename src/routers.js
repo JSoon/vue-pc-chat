d3d5e8cfa1c4ff422d52503ee5753b6baea4c3e7
J.Soon <serdeemail@gmail.com>
2022-03-07 14:42:36 +0800

    refactor: 新建demo分支用于测试

--- src/routers.js
+++ src/routers.js
@@ -1,8 +1,8 @@
-import HomePage from './ui/main/HomePage'
-import LoginPage from './ui/main/LoginPage'
-import ConversationPage from './ui/main/ConversationPage'
-import ContactPage from './ui/main/ContactPage'
-import SettingPage from './ui/main/SettingPage'
+import HomePage from "./ui/main/HomePage";
+import LoginHaiLuo from "./ui/main/LoginHaiLuo";
+import ConversationPage from "./ui/main/ConversationPage";
+import ContactPage from "./ui/main/ContactPage";
+import SettingPage from "./ui/main/SettingPage";
 import Single from "@/ui/voip/Single";
 import Multi from "@/ui/voip/Multi";
 import FileRecordPage from "@/ui/fileRecord/FileRecordPage";
@@ -14,75 +14,75 @@ import MessageHistoryPage from "./ui/main/MessageHistoryPage";
 import ConversationMessageHistoryPage from "./ui/main/ConversationMessageHistoryPage";
 
 const routers = [
-    {
-        path: '/',
-        component: LoginPage
-    },
-    {
-        path: '/home',
-        component: HomePage,
-        children: [
-            {
-                path: '/',
-                name: 'conversation',
-                component: ConversationPage,
-            },
-            {
-                path: 'contact',
-                name: 'contact',
-                component: ContactPage,
-            },
-            {
-                path: 'fav',
-                name: 'fav',
-                component: FavPage,
-            },
-            {
-                path: 'setting',
-                name: 'setting',
-                component: SettingPage,
-            },
-        ]
-    },
-    {
-        name: 'voip-single',
-        path: '/voip/single',
-        component: Single,
-    },
-    {
-        name: 'voip-multi',
-        path: '/voip/multi',
-        component: Multi,
-    },
-    {
-        name: 'voip-conference',
-        path: '/voip/conference',
-        component: Conference,
-    },
-    {
-        name: 'files',
-        path: '/files',
-        component: FileRecordPage,
-    },
-    {
-        name: 'workspace',
-        path: '/workspace',
-        component: WorkspacePage,
-    },
-    {
-        name: 'composite-message',
-        path: '/composite',
-        component: CompositeMessagePage,
-    },
-    {
-        name: 'message-history',
-        path: '/message-history',
-        component: MessageHistoryPage,
-    },
-    {
-        name: 'conversation-message-history',
-        path: '/conversation-message-history',
-        component: ConversationMessageHistoryPage,
-    }
-]
-export default routers
+  {
+    path: "/",
+    component: LoginHaiLuo
+  },
+  {
+    path: "/home",
+    component: HomePage,
+    children: [
+      {
+        path: "/",
+        name: "conversation",
+        component: ConversationPage
+      },
+      {
+        path: "contact",
+        name: "contact",
+        component: ContactPage
+      },
+      {
+        path: "fav",
+        name: "fav",
+        component: FavPage
+      },
+      {
+        path: "setting",
+        name: "setting",
+        component: SettingPage
+      }
+    ]
+  },
+  {
+    name: "voip-single",
+    path: "/voip/single",
+    component: Single
+  },
+  {
+    name: "voip-multi",
+    path: "/voip/multi",
+    component: Multi
+  },
+  {
+    name: "voip-conference",
+    path: "/voip/conference",
+    component: Conference
+  },
+  {
+    name: "files",
+    path: "/files",
+    component: FileRecordPage
+  },
+  {
+    name: "workspace",
+    path: "/workspace",
+    component: WorkspacePage
+  },
+  {
+    name: "composite-message",
+    path: "/composite",
+    component: CompositeMessagePage
+  },
+  {
+    name: "message-history",
+    path: "/message-history",
+    component: MessageHistoryPage
+  },
+  {
+    name: "conversation-message-history",
+    path: "/conversation-message-history",
+    component: ConversationMessageHistoryPage
+  }
+];
+export default routers;
---
+++ src/ui/main/LoginHaiLuo.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="login-hailuo">
+    <form @submit.prevent="onLoginSubmit">
+      <div>
+        <label for="loginName">用户名</label>
+        <input id="loginName" type="text" name="account" value="18583399612" />
+      </div>
+      <div>
+        <label for="loginPwd">密码</label>
+        <input id="loginPwd" type="password" name="password" value="198906l2" />
+      </div>
+      <div>
+        <button type="submit">登录</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import wfc from "../../wfc/client/wfc";
+import axios from "axios";
+import JSEncrypt from "jsencrypt";
+import { setItem } from "../util/storageHelper";
+import ConnectionStatus from "../../wfc/client/connectionStatus";
+import EventType from "../../wfc/client/wfcEvent";
+
+axios.defaults.baseURL = "https://hlapitest.jwell56.com";
+axios.defaults.headers.common.client = "hlweb";
+
+// 密码加密key
+const PUBLIC_KEY =
+  "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqqvBIBvv53+8M8CfJx6hdbTr5quw95p+4UgzBYHYbHayl8TjlgtgzZ9tmxSK1BkdB7hnQLxv3zCiItbyVcJGCGj1uuyP9iZeKZZVgs7/T4J8u+eWVIXvNIFqAT/+o5qctC7FZ3Vs1sfjA/30lQ3q3EyF7UgaHJhdyvtyY7QpQ8QIDAQAB";
+
+const encrptPwd = originPwd => {
+  const encrypt = new JSEncrypt();
+  encrypt.setPublicKey(PUBLIC_KEY);
+  return encrypt.encrypt(originPwd);
+};
+
+export default {
+  created() {
+    wfc.eventEmitter.on(
+      EventType.ConnectionStatusChanged,
+      this.onConnectionStatusChange
+    );
+  },
+  beforeDestroy() {
+    wfc.eventEmitter.removeListener(
+      EventType.ConnectionStatusChanged,
+      this.onConnectionStatusChange
+    );
+  },
+  methods: {
+    // 登录提交
+    async onLoginSubmit(e) {
+      const formData = new FormData(e.target);
+      const formProps = Object.fromEntries(formData);
+
+      // 1. 获取access token
+      const { data: loginRes } = await axios.post(
+        "/jplat/hlweb/login/passwordLogin",
+        null,
+        {
+          params: {
+            account: formProps.account,
+            password: encrptPwd(formProps.password)
+          }
+        }
+      );
+      const token = loginRes.data.accessToken;
+      axios.defaults.headers.common["Authorization"] = `bearer ${token}`;
+
+      // 2. 获取用户信息
+      const { data: userInfoRes } = await axios.get(
+        "/jplat/hlweb/getUserByToken"
+      );
+      const userId = userInfoRes.data.id;
+      axios.defaults.headers.common["id"] = userId || "";
+      axios.defaults.headers.common["accountid"] =
+        userInfoRes.data.accountid || "";
+
+      // 3. 获取im token
+      const { data: imRes } = await axios.post("/imserver/im/getToken", null, {
+        params: {
+          userId,
+          clientId: wfc.getClientId(),
+          platform: 5
+        }
+      });
+      const imToken = imRes.data.token;
+
+      setItem("userId", userId);
+      setItem("token", token);
+      setItem("imtoken", imToken);
+
+      wfc.connect(userId, imToken);
+    },
+    // IM连接状态变化事件
+    onConnectionStatusChange(status) {
+      if (status === ConnectionStatus.ConnectionStatusConnected) {
+        this.$router.replace({ path: "/home" });
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
